8f39f4c30f08a777b8751c7b64c2acb6862f9ec6
JP <jp@jp.nl>
2025-08-11 11:09:03 +0200

    Projecten aangemaakt en basic databinding laten zien in Static SSR-project

---
+++ demos/DemoSolution/DemoProject/Entities/Destination.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoProject.Entities;
+
+public class Destination
+{
+    public int Id { get; set; }
+    public string Location { get; set; }
+    public int Rating { get; set; }
+    public string PhotoUrl { get; set; }
+}
