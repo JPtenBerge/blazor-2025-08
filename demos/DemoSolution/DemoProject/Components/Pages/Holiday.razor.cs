bcc7f7bb51b7d822d02b074c30bd3e2ff454821d
JP <jp@jp.nl>
2025-08-11 13:12:46 +0200

    Formulier met validatie laten zien

--- demos/DemoSolution/DemoProject/Components/Pages/Holiday.razor.cs
+++ demos/DemoSolution/DemoProject/Components/Pages/Holiday.razor.cs
@@ -1,10 +1,15 @@
 ﻿using DemoProject.Entities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace DemoProject.Components.Pages;
 
 public partial class Holiday
 {
-    public List<Destination>? Destinations { get; set; } =
+    [SupplyParameterFromForm]
+    public Destination NewDestination { get; set; } = new();
+
+    public static List<Destination>? Destinations { get; set; } =
     [
         new() {Id = 4, Location = "Garmisch-Partenkirchen", PhotoUrl = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.jamesedition.com%2Flisting_images%2F2025%2F08%2F05%2F16%2F27%2F22%2F4221ba56-cc8a-4d6a-960e-fd8e4330cc3a%2Fje%2F1040x620xc.jpg&f=1&nofb=1&ipt=4ee75d1fb1f1d694a03e560c8ad0335f55757801d59eabae7fd0655e684bff99", Rating = 8},
         new() {Id = 8, Location = "Van der Valk Veenendaal", PhotoUrl = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.hotelspecials.de%2F8cf72eff5ca009e6f371fdb3e68cbbdb.jpeg%3Fw%3D800%26h%3D528%26c%3D1%26quality%3D70&f=1&nofb=1&ipt=e40149718cd361915f265f1bfbb2ea2cd17ec29f2770e4b909038ad49120e6f5", Rating = 8},
@@ -12,8 +17,9 @@ public partial class Holiday
     ];
 
 
-    void ChangeName()
+    void AddDestination()
     {
-        Name = "Yoran";
+        Console.WriteLine($"wow! Werkt! {NewDestination.Location}");
+        Destinations!.Add(NewDestination);
     }
 }
--- demos/DemoSolution/DemoProject/Entities/Destination.cs
+++ demos/DemoSolution/DemoProject/Entities/Destination.cs
@@ -1,9 +1,18 @@
-﻿namespace DemoProject.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoProject.Entities;
 
 public class Destination
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Vul in stomme zak")]
+    [RegularExpression("^[A-Z][a-zA-Z0-9 -]*$")]
     public string Location { get; set; }
+
+    [Required]
     public int Rating { get; set; }
+
+    [Required]
     public string PhotoUrl { get; set; }
 }
