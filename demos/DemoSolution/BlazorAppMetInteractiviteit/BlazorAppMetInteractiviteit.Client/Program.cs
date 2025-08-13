74dae00e0c9ff6d9a6ae02e56cd792d6f1f7735b
JP <jp@jp.nl>
2025-08-13 15:15:04 +0200

    Client gegenereerd via Connected Services > Managed Connected Services

--- demos/DemoSolution/BlazorAppMetInteractiviteit/BlazorAppMetInteractiviteit.Client/Program.cs
+++ demos/DemoSolution/BlazorAppMetInteractiviteit/BlazorAppMetInteractiviteit.Client/Program.cs
@@ -7,3 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddTransient<IDestinationRepository, DestinationRepository>();
 
 await builder.Build().RunAsync();
+
+
+//var client = new Hatseflats.Dinges.demoproject__v1Client(null!);
+
