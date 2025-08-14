ca5d6ccbe09c7a3e91a1a5b240c9ea7de06c49ba
JP <jp@jp.nl>
2025-08-14 13:37:02 +0200

    Project gegeneerd, bezig met config

---
+++ demos/DemoSolution/DemoDuende/DemoDuende/WeatherEndpointExtensions.cs
@@ -0,0 +1,11 @@
+namespace DemoDuende
+{
+    public static class WeatherEndpointExtensions
+    {
+        public static void MapWeatherEndpoints(this WebApplication app) => app.MapGet("/WeatherForecast", async (IWeatherClient weatherClient) => await weatherClient.GetWeatherForecasts())
+                .RequireAuthorization()
+                .AsBffApiEndpoint();
+
+
+    }
+}
